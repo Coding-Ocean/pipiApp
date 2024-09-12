2a859c8dd6c6a41cea99bd6d4a3c40f6e6542229
Coding-Ocean <eijo524@gmail.com>
2024-09-12 18:10:59 +0900

    create mesh from text

--- MAIN.cpp
+++ MAIN.cpp
@@ -2,6 +2,7 @@
 #include"../pipiLib/WINDOW.h"
 #include"../pipiLib/GRAPHIC.h"
 #include"../pipiLib/STATIC_MESH.h"
+#include"../pipiLib/primitive.h"
 
 int MAIN()
 {
@@ -10,25 +11,29 @@ int MAIN()
     w.create(L"pipi");
     g.create();
 
+    createVtxSphere(0.5f, 30);
+    createVtxCube(0.5f);
+
     {
-        FLOAT3 eye = { 0, 0, 8 }, focus = { 0, 0, 0 }, up = { 0, 1, 0 };
+        FLOAT3 eye = { 0, 0, 2 }, focus = { 0, 0, 0 }, up = { 0, 1, 0 };
         g.setView(eye, focus, up);
 
         //mesh
         STATIC_MESH mesh;
-        mesh.create("assets/cube.txt");
+        mesh.create("assets/sphere.txt");
         MATRIX world;
         float r = 0.0f;
 
         while (!w.quit()) {
             //update
             world.identity();
+            //world.mulRotateX(0.1f);
             world.mulRotateY(r);
-            world.mulRotateX(r/2);
+            //world.mulRotateX(0.5f);
             r += 0.01f;
             mesh.update(world);
             //draw
-            g.clear(0.25f, 0.5f, 0.9f);
+            g.clear(0.1f, 0.1f, 0.15f);
             g.setPipeline();
             mesh.draw();
             g.present();
